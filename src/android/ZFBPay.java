4cced50b5fdf5cc04ac07e030af93492701bdbde
egou <egou@hotmail.com>
2014-05-04 21:19:00 +0800

    add ZFB

---
+++ src/android/ZFBPay.java
@@ -0,0 +1,160 @@
+﻿package cn.lovetennis.zfb;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.app.Activity;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alipay.android.app.sdk.AliPay;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+public class ZFBPay extends CordovaPlugin{
+	public static final String TAG = "alipay-sdk";
+
+	private static final int RQF_PAY = 1;
+
+	private static final int RQF_LOGIN = 2;
+
+	private CallbackContext callbacks = null;
+
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject obj = args.getJSONObject(0);
+		callbacks = callbackContext;
+		Activity act = this.cordova.getActivity();
+		try {
+			String info = getNewOrderInfo();
+			String sign = Rsa.sign(info, Keys.PRIVATE);
+			sign = URLEncoder.encode(sign);
+			info += "&sign=\"" + sign + "\"&" + getSignType();
+			Log.i("ExternalPartner", "start pay");
+			// start the pay.
+			Log.i(TAG, "info = " + info);
+
+			final String orderInfo = info;
+			new Thread() {
+				public void run() {
+					AliPay alipay = new AliPay(ZFBPay.this.cordova.getActivity(), mHandler);
+					
+					//设置为沙箱模式，不设置默认为线上环境
+					//alipay.setSandBox(true);
+
+					String result = alipay.pay(orderInfo);
+
+					Log.i(TAG, "result = " + result);
+					Message msg = new Message();
+					msg.what = RQF_PAY;
+					msg.obj = result;
+					mHandler.sendMessage(msg);
+				}
+			}.start();
+			return true;
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			Toast.makeText(ZFBPay.this.cordova.getActivity(), "failed to invoke",
+					Toast.LENGTH_SHORT).show();
+			return false;
+		}
+	}
+
+	private String getNewOrderInfo() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("partner=\"");
+		sb.append(Keys.DEFAULT_PARTNER);
+		sb.append("\"&out_trade_no=\"");
+		sb.append(getOutTradeNo());
+		sb.append("\"&subject=\"");
+		sb.append("booking");
+		sb.append("\"&body=\"");
+		sb.append("booking court from 1pm to 3 pm for 2 hours");
+		sb.append("\"&total_fee=\"");
+		sb.append("0.01");
+		sb.append("\"&notify_url=\"");
+
+		// 网址需要做URL编码
+		sb.append(URLEncoder.encode("http://app.lovetennis.cn/zfb/notify.wqb"));
+		sb.append("\"&service=\"mobile.securitypay.pay");
+		sb.append("\"&_input_charset=\"UTF-8");
+		sb.append("\"&return_url=\"");
+		sb.append(URLEncoder.encode("http://app.lovetennis.cn"));
+		sb.append("\"&payment_type=\"1");
+		sb.append("\"&seller_id=\"");
+		sb.append(Keys.DEFAULT_SELLER);
+
+		// 如果show_url值为空，可不传
+		// sb.append("\"&show_url=\"");
+		sb.append("\"&it_b_pay=\"1m");
+		sb.append("\"");
+
+		return new String(sb);
+	}
+
+	private String getOutTradeNo() {
+		SimpleDateFormat format = new SimpleDateFormat("MMddHHmmss");
+		Date date = new Date();
+		String key = format.format(date);
+
+		java.util.Random r = new java.util.Random();
+		key += r.nextInt();
+		key = key.substring(0, 15);
+		Log.d(TAG, "outTradeNo: " + key);
+		return key;
+	}
+
+	private String getSignType() {
+		return "sign_type=\"RSA\"";
+	}
+
+	Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			Result result = new Result((String) msg.obj);
+
+			switch (msg.what) {
+			case RQF_PAY:
+			case RQF_LOGIN: {
+				Toast.makeText(ZFBPay.this.cordova.getActivity(), result.getResult(),
+						Toast.LENGTH_SHORT).show();
+
+			}
+				break;
+			default:
+				break;
+			}
+		};
+	};
+
+}
---
+++ www/ZFBPay.js
@@ -0,0 +1,14 @@
+var zfbpay = {
+    pay: function(params, success, failure) {
+        cordova.exec(
+            success, // success callback function
+            failure, // error callback function
+            'zfbpay', // mapped to our native Java class called "CalendarPlugin"
+            'pay', // with this action name
+            [
+				params
+			]
+        ); 
+    }
+}
+module.exports = wxpay;
